20c8824d81951ff17e61be25d4e3f2d3cf79c5a1
Sveta <sveta@svetakotleta.ru>
2017-07-04 14:29:16 +0300

    Update guava.c - use long long

--- guava.c
+++ guava.c
@@ -2,19 +2,18 @@
 #include <stdint.h>
 #include "guava.h"
 
-static const long
-	K   = 2862933555777941757L,
-	M64 = 0xffffffffffffffffL;
+static const long long
+	K   = 2862933555777941757L;
 
 static const double
 	D   = 0x1.0p31;
 
-int guava(long state, unsigned int buckets) {
+int guava(long long state, unsigned int buckets) {
 	int candidate = 0;
 	int next;
 	while (1) {
 		state = K * state + 1;
-		next = (int) ( (double) (candidate + 1) / ( (double)( (int)( (long unsigned) state >> 33 ) + 1 ) / D ) );
+		next = (int) ( (double) (candidate + 1) / ( (double)( (int)( (long long unsigned) state >> 33 ) + 1 ) / D ) );
 		if ( ( next >= 0 ) && ( next < buckets )) {
 			candidate = next;
 		} else {
@@ -22,4 +21,3 @@ int guava(long state, unsigned int buckets) {
 		}
 	}
 }
-
